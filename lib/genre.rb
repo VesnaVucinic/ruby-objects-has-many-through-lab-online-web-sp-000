5638288be739a026c26d8fbf5f93a12da9dec218
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 21:33:11 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,8 +13,8 @@ class Genre
   end
 
   def songs
-    Song.all { |song| song.genre == self }
-    song.all.select do |song|
+    #Song.all { |song| song.genre == self }
+    Song.all.select do |song|
       song.genre == self
 
   end
