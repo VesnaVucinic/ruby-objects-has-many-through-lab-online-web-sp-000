3346cf9d9ef3f5041bd2f55b1c42c2fc15f8f5b7
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 21:50:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,19 @@
 class Song 
+  attr_accessor :name 
+  
+  @@all =[]
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
+
   
