a61ed511ab92713115355afd4c3e998e55bb237b
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 22:17:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,12 +12,12 @@ class Artist
     @@all
   end
   
-  #The Artist class needs an instance method, #new_song, that takes in an argument of a name and genre creates a new song. That song should know that it belongs to the artist.
+  #The Artist class needs an instance method, #new_song, #that takes in an argument of a name and genre creates #a new song. That song should know that it belongs to #the artist.
   def new_song (name, genre)
     Song.new(name, self, genre)
   end
 
-#The Artist class needs an instance method, #songs, that iterates through all songs and finds the songs that belong to that artist. Try using select to achieve this.
+#The Artist class needs an instance method, #songs, that #iterates through all songs and finds the songs that #belong to that artist. Try using select to achieve this.
   def songs 
     song.all.select do |song|
       song.artist == self
