feb8674134c86bae924f27bc2b7de3f542475c0d
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 23:02:33 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,28 @@
 class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(doctor, date, self)
+  end
+  
+  def appointments
+    Appointment.all.collect {|Appointment| Appointment.patient == self}
+  end
+  
+  def doctors
+    Appointment.map(&:doctor)
+  end
+end
+
   
