acbdb052b6cb2493413745b5d60aea918d9a8485
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 22:36:09 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,16 @@
+class Doctor
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment
+    Patient
+  
