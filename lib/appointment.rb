e834f50f331f29282d6d1bd2a963305741ed6347
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 23:08:06 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,12 +1,19 @@
+Learn more or give us feedback
 class Appointment
-  attr_accessor 
-  
-  @@all =[]
-  
-  def initialize()
-  
-  def self.all
-    @all
+  attr_accessor :date, :doctor, :patient
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+end
   
   
