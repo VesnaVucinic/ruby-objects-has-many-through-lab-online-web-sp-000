05f8b9eb6a05d7674086b97bd47bb32db0a506ce
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-11 21:23:52 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,34 @@
-class Artist
-  attr_accessor :name
-
+class Artist 
+  attr_accessor :name 
+  
   @@all = []
-
+  
   def initialize(name)
     @name = name
     @@all << self
   end
-
+  
   def self.all
     @@all
   end
-
-  def new_song(name, genre)
-    Song.new(name, self, genre)
+  
+  def new_song (name, genre)
+  #    Song.new(name, self, genre)
+    Song.new(name, genre)
+    song.atrist == self
   end
 
-  def songs
-    Song.all.select { |song| song.artist == self }
+  def songs 
+    song.all.select do |song|
+      song.artist == self
+    end
   end
-
-  def genres
-    songs.map(&:genre)
+  
+  def genres 
+    song.all.collect do |genre|
+      genre.song 
+    #songs.map(&:genre)
+    end
   end
+  
 end
